b598eed1a55c6a31d93aea6794b19bb543929315
Abinash kumar <163131540+abinashkumar19@users.noreply.github.com>
2025-10-08 12:51:52 +0530

    Update terraform.sh

--- terraform/terraform.sh
+++ terraform/terraform.sh
@@ -1,11 +1,11 @@
-# Download Terraform 1.6.1 (latest stable as of now)
-curl -LO https://releases.hashicorp.com/terraform/1.6.1/terraform_1.6.1_linux_amd64.zip
+# Download latest Terraform for Linux
+curl -LO https://releases.hashicorp.com/terraform/1.13.3/terraform_1.13.3_linux_amd64.zip
 
-# Unzip the binary
-unzip terraform_1.6.1_linux_amd64.zip
+# Unzip it
+unzip terraform_1.13.3_linux_amd64.zip
 
-# Move it to /usr/local/bin so it's globally accessible
+# Move binary to /usr/local/bin
 sudo mv terraform /usr/local/bin/
 
-# Verify installation
+# Check version
 terraform version
